926c3ffa2815d440f571c6efd04778ce9870e238
masaya09 <74097011+masaya09@users.noreply.github.com>
2023-04-03 02:16:41 +0900

    Netlifyに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  site: 'https://will-promotion.github.io',
-  base: '/will-promotion.github.io',
+  output: 'server',
+  adapter: netlify(),
 });
