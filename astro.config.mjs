cc97fce874f0e44057cd324d64a748197c81671b
masaya09 <74097011+masaya09@users.noreply.github.com>
2023-04-03 01:21:06 +0900

    使用してないimport削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://will-promotion.github.io',
   base: '/will-promotion.github.io',
-  integrations: [react()],
 });
