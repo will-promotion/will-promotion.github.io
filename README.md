32ac26ca3ebda2db43b267531e452eeb9a31c278
masaya09 <74097011+masaya09@users.noreply.github.com>
2023-04-03 10:33:20 +0900

    GitHub Pagesに変更

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # will-promotion
-2023年版 新歓サイト公開URL：https://will-promotion.netlify.app/
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  site: 'https://will-promotion.github.io'
 });
